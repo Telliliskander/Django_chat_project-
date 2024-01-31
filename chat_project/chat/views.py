20288a234df77b580d5aa5a8137ad693fa49a550
tuto <skander.tlili@enstab.ucar.tn>
2024-01-31 13:03:10 +0100

    at this point i've tried to load my first page

--- chat_project/chat/views.py
+++ chat_project/chat/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def message_page(request):
+    return render(request, 'start_point_messages.html') 
--- chat_project/chat_project/settings.py
+++ chat_project/chat_project/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent # = Django_chat_project/chat_project
 
 
 # Quick-start development settings - unsuitable for production
@@ -116,7 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'chat/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
